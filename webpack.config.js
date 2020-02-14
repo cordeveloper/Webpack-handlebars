7af5e4d61f0500aca0ee79b54e51de968b19232f
cordeveloper <cesar.cordera@larefactoria.com>
2020-02-14 13:11:29 +0100

    zip options & cross-env

--- webpack.config.js
+++ webpack.config.js
@@ -4,149 +4,146 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const WebpackNotifier = require('webpack-notifier');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserWebpackPlugin = require('terser-webpack-plugin');
 const webpack = require('webpack');
+const ZipPlugin = require('zip-webpack-plugin');
 
 
+const dev = process.env.NODE_ENV !== 'production';
+const prod = !dev;
 
-module.exports = env => {  
-  
-  const dev = env.development;
-  const prod = !env.development;
+module.exports = {
 
-  return {
-    entry: ['@webcomponents/webcomponentsjs','./src/app.js'],
-    output: {
-      path:  path.resolve(__dirname,'dist'),
-      filename: 'bundle.js'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(sa|sc|c)ss$/,
-          use: [
-            {
-              loader:   MiniCssExtractPlugin.loader,
-              options: {
-                hmr: dev,
-                reloadAll: true,
-                publicPath: '../'
-              }
-            },
-    
-            'css-loader', 'postcss-loader', 'sass-loader'
-          ]
-        },
-        {
-          test: /\.(jpg|png|gif|jpeg|svg)$/,
-          exclude: [/webfonts/],
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                name: '[name].[ext]',
-                outputPath: './assets/img',
-                esModule: false,
-              }
+  entry: ['@webcomponents/webcomponentsjs', './src/app.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
+  },
+  module: {
+    rules: [{
+        test: /\.(sa|sc|c)ss$/,
+        use: [{
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: dev,
+              reloadAll: true,
+              publicPath: '../'
             }
-          ]
-        },
-        {
-          loader: 'image-webpack-loader',
+          },
+
+          'css-loader', 'postcss-loader', 'sass-loader'
+        ]
+      },
+      {
+        test: /\.(jpg|png|gif|jpeg|svg)$/,
+        exclude: [/webfonts/],
+        use: [{
+          loader: 'file-loader',
           options: {
-            disable: dev,
-            mozjpeg: {
-              progressive: true,
-              quality: 65
-            },
-            // optipng.enabled: false will disable optipng
-            optipng: {
-              enabled: true,
-            },
-            pngquant: {
-              quality: [0.65, 0.90],
-              speed: 4
-            },
-            gifsicle: {
-              interlaced: false,
-            },
-            // the webp option will enable WEBP
-            webp: {
-              quality: 75
-            }
+            name: '[name].[ext]',
+            outputPath: './assets/img',
+            esModule: false,
+          }
+        }]
+      },
+      {
+        loader: 'image-webpack-loader',
+        options: {
+          disable: dev,
+          mozjpeg: {
+            progressive: true,
+            quality: 65
+          },
+          // optipng.enabled: false will disable optipng
+          optipng: {
+            enabled: true,
+          },
+          pngquant: {
+            quality: [0.65, 0.90],
+            speed: 4
+          },
+          gifsicle: {
+            interlaced: false,
+          },
+          // the webp option will enable WEBP
+          webp: {
+            quality: 75
+          }
+        }
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot|svg)$/i,
+        exclude: [/img/, /image/],
+        use: [{
+          loader: 'file-loader',
+          options: {
+            outputPath: 'assets/webfonts',
+            name: '[name].[ext]'
+          }
+        }]
+      },
+      {
+        test: /\.handlebars$/,
+        use: [{
+          loader: "handlebars-loader",
+          query: {
+            inlineRequires: '/assets/img/'
+          }
+        }, ]
+      },
+      {
+        test: /\.js$/,
+        exclude: '/node_modules/',
+        loader: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env'
+            ]
           }
         },
-        {
-          test: /\.(ttf|woff|woff2|eot|svg)$/i,
-          exclude: [/img/,/image/],
-          use: [
-              {
-                  loader: 'file-loader',
-                  options: {
-                      outputPath: 'assets/webfonts',
-                      name: '[name].[ext]'
-                  }
-              }
-          ]
-        },
-        {
-          test: /\.handlebars$/,
-          use: [
-            {
-              loader: "handlebars-loader",
-              query: {
-                inlineRequires: '/assets/img/'
-              }
-            },
-          ]
-        },
-        { test: /\.js$/, 
-          exclude: '/node_modules/', 
-          loader: {
-              loader: 'babel-loader',
-              options: {
-                  presets: [
-                      '@babel/preset-env'
-                  ]
-              }
-          },
-          
-        }
-        
-      ]
-    },
-    plugins: [
-      new webpack.ProgressPlugin(),
-      new CleanWebpackPlugin(),
-      new MiniCssExtractPlugin({
-        filename: 'css/style.css'
-      }),
-      new HtmlWebpackPlugin({
-        template: './src/index.handlebars',
-        minify: {
-          collapseWhitespace: prod,
-          removeComments: prod,
-          removeRedundantAttributes: prod,
-          removeScriptTypeAttributes: prod,
-          removeStyleLinkTypeAttributes: prod,
-          useShortDoctype: prod
-        }
-      }),
-      new WebpackNotifier(
-        {contentImage: path.resolve(__dirname, '../webpack.png')}
-    ),
-    ],
-    devServer: {
-      port: 4200,
-      //hot: dev
-    },
-   /*  optimization: {
-      splitChunks: {
-        chunks: dev ? 'async' : 'all'
+
       }
-    } */
-  }
-   
+
+    ]
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/style.css'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.handlebars',
+      minify: {
+        collapseWhitespace: prod,
+        removeComments: prod,
+        removeRedundantAttributes: prod,
+        removeScriptTypeAttributes: prod,
+        removeStyleLinkTypeAttributes: prod,
+        useShortDoctype: prod
+      }
+    }),
+    new WebpackNotifier({
+      contentImage: path.resolve(__dirname, '../webpack.png')
+    }),
+  ],
+  devServer: {
+    port: 4200,
+    //hot: dev
+  },
+  /*  optimization: {
+     splitChunks: {
+       chunks: dev ? 'async' : 'all'
+     }
+   } */
+
+
 }
- 
+
+if(prod){
+  module.exports.plugins.push(
+    new ZipPlugin({
+      filename: 'dist.zip'
+    })
+  )
+}
